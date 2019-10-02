b64c72046dd4558a7a2fd63cbe92fdd7f513c671
Conor Fox <conor@conorfox.com>
2019-10-02 22:09:39 +0100

    Error check complete

--- Lab3/functions.py
+++ Lab3/functions.py
@@ -11,10 +11,13 @@ def three_numbers(num1, num2, num3):
     num2 -- Second imputted number
     num3 -- Third imputted number
     """
-    if num1 == num2 and num2 == num3:
-        print("True")
+    if str(type(num1)) == "<class 'int'>" and str(type(num2)) == "<class 'int'>" and str(type(num3)) == "<class 'int'>":
+        if num1 == num2 == num3:
+            print("True")
+        else:
+            print("False")
     else:
-        print("False")
+        print("Error - Input is not an integer")
 
 
 def seasons(number):
@@ -31,7 +34,7 @@ def seasons(number):
     elif number == 4:
         print("Winter")
     else:
-        print("Error please enter an integer between 1 and 4")
+        print("Error - please enter an integer between 1 and 4")
 
 
 def grades(score):
@@ -39,31 +42,44 @@ def grades(score):
     Gives grade based on test score
     score -- Test score between 0 and 100
     """
-    if score >= 0 and score <= 24:
-        print("F")
-    elif score >= 25 and score <= 39:
-        print("E")
-    elif score >= 40 and score <= 54:
-        print("D")
-    elif score >= 55 and score <= 69:
-        print("C")
-    elif score >= 70 and score <= 84:
-        print("B")
-    elif score >= 85 and score <= 100:
-        print("A")
+    if str(type(score)) == "<class 'int'>":
+        if score >= 0 and score <= 24:
+            print("F")
+        elif score >= 25 and score <= 39:
+            print("E")
+        elif score >= 40 and score <= 54:
+            print("D")
+        elif score >= 55 and score <= 69:
+            print("C")
+        elif score >= 70 and score <= 84:
+            print("B")
+        elif score >= 85 and score <= 100:
+            print("A")
+        else:
+            print("X")
     else:
-        print("X")
+        print("Error - please enter an integer between 0 and 100")
 
 
 def equal_numbers(num1, num2):
-    if num1 == num2:
-        square_root = math.sqrt(num1)
-        print("The square root of %d is %d" % (int(num1), int(square_root)))
-    else:
-        square_num1 = num1 ** 2
-        square_num2 = num2 ** 2
+    """
+    Takes two numbers, if equal gives the square root, if different gives square of both numbers
+    num1 -- First imputted number
+    num2 -- Second imputted number
+    """
+    if str(type(num1)) == "<class 'int'>" and str(type(num2)) == "<class 'int'>":
+        if num1 == num2:
+            square_root = math.sqrt(num1)
+            print("The square root of %d is %.3f" %
+                  (int(num1), float(square_root)))
+        else:
+            square_num1 = num1 ** 2
+            square_num2 = num2 ** 2
 
-        print("\"%d\" squared is \"%d\" and \"%d\" squared is \"%d\"" % (num1, square_num1, num2, square_num2))
+            print("\"%d\" squared is \"%d\" and \"%d\" squared is \"%d\"" %
+                  (num1, square_num1, num2, square_num2))
+    else:
+        print("Error - Input is not an integer")
 
 
 def fizz_buzz(number):
@@ -71,11 +87,14 @@ def fizz_buzz(number):
     Like game of FizzBuzz, if number is a multiple of 3 prints Fizz, if number is a multiple of 5 prints Buzz and if number is a multiple of both prints FizzBuzz
     number -- Number to be inputted
     """
-    if (number % 5) == 0 and (number % 3) == 0:
-        print("FizzBuzz")
-    elif (number % 3) == 0:
-        print("Fizz")
-    elif (number % 5) == 0:
-        print("Buzz")
+    if str(type(number)) == "<class 'int'>":
+        if (number % 5) == 0 and (number % 3) == 0:
+            print("FizzBuzz")
+        elif (number % 3) == 0:
+            print("Fizz")
+        elif (number % 5) == 0:
+            print("Buzz")
+        else:
+            print(number)
     else:
-        print(number)
+        print("Error - Input is not an integer")
--- Lab3/main.py
+++ Lab3/main.py
@@ -5,7 +5,22 @@ import functions
 
 
 def main():
+    # functions.three_numbers("one", "two", "three")
+    functions.seasons(4.5)
+    functions.seasons("hello")
+    functions.equal_numbers(16, 16)
+    functions.equal_numbers(4, 9)
+    functions.equal_numbers("five", 5)
+    functions.seasons(3)
+    functions.seasons(6)
+    functions.grades(99)
+    functions.grades(144)
+    functions.grades("hello")
+    functions.three_numbers(1, 2, 3)
     functions.three_numbers("one", "two", "three")
+    functions.three_numbers(1, 1, 1)
+    functions.fizz_buzz(15)
+    functions.fizz_buzz("hrllo")
 
 
 if __name__ == '__main__':
