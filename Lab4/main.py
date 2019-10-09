31cc3a2ae1d271beb313e155ea9e4dade4698de7
Conor Fox <conor@conorfox.com>
2019-10-09 17:29:17 +0100

    Main

---
+++ Lab4/main.py
@@ -0,0 +1,40 @@
+# ScriptName: main.py
+# Author: Conor Fox 119322236
+
+import functions
+
+
+def main():
+    '''
+    functions.seasons(1)
+    functions.seasons(2)
+    functions.seasons(3)
+    functions.seasons(4)
+    functions.seasons(5)
+    functions.seasons("a")
+    '''
+    '''
+    functions.grades("F")
+    functions.grades("E")
+    functions.grades("D")
+    functions.grades("C")
+    functions.grades("B")
+    functions.grades("A")
+    functions.grades(10)
+    functions.grades(30)
+    functions.grades(60)
+    functions.grades(80)
+    functions.grades(99)
+    functions.grades(300)
+    '''
+    '''
+    functions.fizz_buzz(15, 3, 5)
+    functions.fizz_buzz(20, 10, 6)
+    functions.fizz_buzz(81, 8, 9)
+    '''
+    functions.slice_reverse("Racecar")
+    functions.slice_reverse("r", "o", "t", "a", "v", "a", "t", "o", "r”)
+
+
+if __name__ == "__main__":
+    main()
