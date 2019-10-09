dc6b12f704ebd616c19e7dd595db629ab7480cbc
Conor Fox <conor@conorfox.com>
2019-10-09 16:50:46 +0100

    Grades

--- Lab4/functions.py
+++ Lab4/functions.py
@@ -19,3 +19,43 @@ def seasons(number):
         print(seasons_list[2])
     else:
         print("Error - please enter an integer between 1 and 4")
+
+
+def grades(score):
+    """
+    Gives grade based on test score
+    score -- Test score between 0 and 100
+    """
+    if str(type(score)) == "<class 'int'>":
+        if score >= 0 and score <= 24:
+            print("F")
+        elif score >= 25 and score <= 39:
+            print("E")
+        elif score >= 40 and score <= 54:
+            print("D")
+        elif score >= 55 and score <= 69:
+            print("C")
+        elif score >= 70 and score <= 84:
+            print("B")
+        elif score >= 85 and score <= 100:
+            print("A")
+        else:
+            print("The input numerical grade is outside the supported range.")
+
+    elif str(type(score)) == "<class 'str'>":
+        if score == "F":
+            print("0-24")
+        elif score == "E":
+            print("25-39")
+        elif score == "D":
+            print("40-54")
+        elif score == "C":
+            print("55-69")
+        elif score == "B":
+            print("70-84")
+        elif score == "A":
+            print("85-100")
+        else:
+            print("The input letter grade is outside the supported range.")
+    else:
+        print("Error - please enter an integer between 0 and 100 or a letter between A and F")
