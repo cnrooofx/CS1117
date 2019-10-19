556a0f2838cca2939452a2b60f9b13eb38d12a3f
Conor Fox <conor@conorfox.com>
2019-10-19 09:55:09 +0100

    Lab 6

--- Lab6/functions.py
+++ Lab6/functions.py
@@ -2,5 +2,59 @@
 # Author: Conor Fox 119322236
 
 
-def function():
-    return
+def count(list1, value):
+    i = 0
+    accum = 0
+    while i < len(list1):
+        if value == list1[i]:
+            accum += 1
+            i += 1
+            continue
+        i += 1
+    print(accum)
+
+
+def index(list1, value):
+    if value in list1:
+        print(list1.index(value))
+    else:
+        print(-1)
+
+
+def get_value(list1, index):
+    if index in range(len(list1)):
+        print(list1[index])
+    else:
+        print(index, "is not an index in", list1)
+
+
+def insert(list1, index, value):
+    if type(list1) is str:
+        if index in range(len(list1)):
+            start = list1[:index]
+            end = list1[index+1:]
+            print(start + value + end)
+        else:
+            print(list1 + value)
+    elif type(list1) is list:
+        value = [value]
+        if index in range(len(list1)):
+            start = list1[:index]
+            end = list1[index+1:]
+            print(start + value + end)
+        else:
+            print(list1 + value)
+    else:
+        print("list1 is neither 'str' nor 'list' and is therefore not indexable and reassignable")
+
+
+def value_in_list(list1, value):
+    if value in list1:
+        out = True
+    else:
+        out = False
+    return out
+
+
+def concat(list1, list2):
+    print(list1 + list2)
--- Lab6/main.py
+++ Lab6/main.py
@@ -5,7 +5,19 @@ import functions
 
 
 def main():
-    functions.function()
+    '''
+    # functions.count([1, 2, 3, 4, 5, 2, 5, 2, 1], 2)
+    functions.index([1, 2, 3, 4, 5, 2, 5, 2, 1], 2)
+    functions.index("hello", "o")
+    functions.index("hello", "p")
+    functions.get_value("hello", 7)
+    functions.insert("hello", 1, "a")
+    functions.insert("hello", 7, "a")
+    functions.insert([1, 2, 3, 4, 5, 2, 5, 2, 1], 2, 7)
+    functions.insert([1, 2, 3, 4, 5, 2, 5, 2, 1], 12, 7)
+    '''
+    print(functions.value_in_list("hello", "o"))
+    print(functions.value_in_list("hello", "p"))
 
 
 if __name__ == "__main__":
