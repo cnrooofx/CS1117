93956c85ff94daa3b61e91d276b1fb7843340ce1
Conor Fox <conor@conorfox.com>
2019-11-15 21:53:37 +0000

    Remove wildcard * imports

--- CA/main.py
+++ CA/main.py
@@ -1,7 +1,7 @@
 # Script Name: main.py
 # Author: Conor Fox 119322236
 
-from functions import *
+import functions
 
 
 def main():
@@ -10,49 +10,48 @@ def main():
     print(to_engligh(314))
     """
     '''
-    print(to_english(142))  # -> “One hundred and forty two”
-    print(to_english(-142))  # -> “Minus one hundred and forty two”
-    print(to_english(11))  # -> “Eleven"
-    print(to_english(42))  # -> “Forty two"
-    print(to_english(9))  # -> “Nine"
-    print(to_english(716))  # -> “Nine hundred and ninety nine"
-    print(to_english(305))  # -> Minus eleven
-    print(to_english(0))  # -> Zero
-    print(to_english(-9))  # -> Minus nine
-    print(to_english(-999))  # -> Minus nine hundred and ninety nine
-    print(to_english(10))  # -> Ten
-    print(to_english(70))  # -> Seventy
-    print(to_english(-1))  # -> Minus one
+    print(functions.to_english(142))  # -> “One hundred and forty two”
+    print(functions.to_english(-142))  # -> “Minus one hundred and forty two”
+    print(functions.to_english(11))  # -> “Eleven"
+    print(functions.to_english(42))  # -> “Forty two"
+    print(functions.to_english(9))  # -> “Nine"
+    print(functions.to_english(716))  # -> “Nine hundred and ninety nine"
+    print(functions.to_english(305))  # -> Minus eleven
+    print(functions.to_english(0))  # -> Zero
+    print(functions.to_english(-9))  # -> Minus nine
+    print(functions.to_english(-999))  # -> Minus nine hundred and ninety nine
+    print(functions.to_english(10))  # -> Ten
+    print(functions.to_english(70))  # -> Seventy
+    print(functions.to_english(-1))  # -> Minus one
     i = 0
     while i < 1000:
-        print(to_english(i))
+        print(functions.to_english(i))
         i += 1
     '''
     '''
-    print(sort_a_list([2, -3]))  # -> [-3,2]
-    print(sort_a_list([2, 3, 2]))  # -> [2,3]
-    print(sort_a_list([2, 3, 1]))  # -> [1,2,3]
-    print(sort_a_list(['z', 'a']))  # -> ['a', 'z']
-    print(sort_a_list(['H', 'B']))  # -> ['B', 'H']
-    print(sort_a_list(['a', 'B']))  # -> ['B', 'a']
-    print(sort_a_list([1, 3, 6, 8, 3, 2, 0]))
-    print(sort_a_list(["a", "j", "d", "g", "e", "f", "g", "r"]))
-    print(sort_a_list(["A", "j", "d", "G", "e", "f", "g", "r"]))
-    print(sort_a_list(['M', 'p', 'V', 'Z', 'V', 'q', 'D', 'q', 'B', 'B',
+    print(functions.sort_a_list([2, -3]))  # -> [-3,2]
+    print(functions.sort_a_list([2, 3, 2]))  # -> [2,3]
+    print(functions.sort_a_list([2, 3, 1]))  # -> [1,2,3]
+    print(functions.sort_a_list(['z', 'a']))  # -> ['a', 'z']
+    print(functions.sort_a_list(['H', 'B']))  # -> ['B', 'H']
+    print(functions.sort_a_list(['a', 'B']))  # -> ['B', 'a']
+    print(functions.sort_a_list([1, 3, 6, 8, 3, 2, 0]))
+    print(functions.sort_a_list(["a", "j", "d", "g", "e", "f", "g", "r"]))
+    print(functions.sort_a_list(["A", "j", "d", "G", "e", "f", "g", "r"]))
+    print(functions.sort_a_list(['M', 'p', 'V', 'Z', 'V', 'q', 'D', 'q', 'B', 'B',
                        'n', 'k', 'H', 'm', 'p', 'K', 'W', 'j', 'Y', 'Z']))
     '''
     '''
-    print(ascii_difference([1, 2], [3, 4]))  # -> ([100, 102], [2, 2])
-    print(ascii_difference(['a', 2], [3, 4]))  # -> ([148, 102], [46, 2])
-    print(ascii_difference([1, 'b'], [3, 4]))  # -> ([100, 150], [2, 46])
-    print(ascii_difference(['1', '2', '3'], []))  # -> ([49, 50, 51], [49, 50, 51])
-    print(ascii_difference([1, 2], [3, 'C']))  # -> ([100, 117], [2, 17])
-    print(ascii_difference(['a', 'b'], ['z', 'd']))  # -> ([219, 198], [25, 2])
+    print(functions.ascii_difference([1, 2], [3, 4]))  # -> ([100, 102], [2, 2])
+    print(functions.ascii_difference(['a', 2], [3, 4]))  # -> ([148, 102], [46, 2])
+    print(functions.ascii_difference([1, 'b'], [3, 4]))  # -> ([100, 150], [2, 46])
+    print(functions.ascii_difference(['1', '2', '3'], []))  # -> ([49, 50, 51], [49, 50, 51])
+    print(functions.ascii_difference([1, 2], [3, 'C']))  # -> ([100, 117], [2, 17])
+    print(functions.ascii_difference(['a', 'b'], ['z', 'd']))  # -> ([219, 198], [25, 2])
     '''
-    print(ascii_difference([1, 2], [3, 4]))
-    print(ascii_difference(['a', 2], [3, 4]))
+    print(functions.ascii_difference([1, 2], [3, 4]))
+    print(functions.ascii_difference(['a', 2], [3, 4]))
 
 
 if __name__ == "__main__":
-    ''' call the main() function in this file '''
     main()
