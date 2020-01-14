8f187537480c851c31bf15bf81095f3157851718
Conor Fox <conor@conorfox.com>
2020-01-14 08:30:31 +0000

    Grades & Slice Reverse

--- Sem2/Lab1/functions.py
+++ Sem2/Lab1/functions.py
@@ -15,3 +15,34 @@ def seasons(number):
             return seasons[number]
         return "Number entered, "+str(number)+", is outside of input values"
     return "Input value must be a number"
+
+
+def grades(score):
+    if type(score) == int:
+        if score >= 0 and score <= 24:
+            return "F"
+        elif score >= 25 and score <= 39:
+            return "E"
+        elif score >= 40 and score <= 54:
+            return "D"
+        elif score >= 55 and score <= 69:
+            return "C"
+        elif score >= 70 and score <= 84:
+            return "B"
+        elif score >= 85 and score <= 100:
+            return "A"
+        return "The input numerical grade is outside the supported range."
+    elif type(score) == str:
+        grade = {"A": "85-100", "B": "70-84", "C": "55-69", "D": "40-54",
+                 "E": "25-39", "F": "0-24"}
+        if score in grade:
+            return grade[score]
+        return "The input letter grade is outside the supported range."
+    return "Please enter an integer 0 - 100 or a letter A - F"
+
+
+def slice_reverse(input_value):
+    input_value = str(input_value)
+    if input_value == input_value[::-1]:
+        return True
+    return False
--- Sem2/Lab1/main.py
+++ Sem2/Lab1/main.py
@@ -5,6 +5,7 @@ import functions
 
 
 def main():
+    '''
     print("\t Seasons")
     print(functions.seasons(1))
     print(functions.seasons(2))
@@ -16,6 +17,21 @@ def main():
     print(functions.three_numbers(1, 2, 3))
     print(functions.three_numbers(1, 1, "a"))
     print(functions.three_numbers(2, 2, 2))
+    print("\t Grades")
+    print(functions.grades("F"))
+    print(functions.grades("E"))
+    print(functions.grades("D"))
+    print(functions.grades("C"))
+    print(functions.grades("B"))
+    print(functions.grades("A"))
+    print(functions.grades(10))
+    print(functions.grades(30))
+    print(functions.grades(60))
+    print(functions.grades(80))
+    print(functions.grades(99))
+    print(functions.grades(300))
+    '''
+    print(functions.slice_reverse([1, 2, 3, 2, 1]))
 
 
 if __name__ == "__main__":
