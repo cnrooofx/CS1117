06a3717f325eedb989269e6fbdda261b7e0b7389
Conor <conor@conorfox.com>
2020-02-03 16:04:44 +0000

    Lab 3 update

--- Sem2/Lab3/functions.py
+++ Sem2/Lab3/functions.py
@@ -6,7 +6,8 @@
 
 def removeVowels(sentence):
     vowels = "aeiou"
-    return " ".join([l for l in sentence if l not in vowels])
+    filtered_list = [l for l in sentence if l not in vowels]
+    return " ".join(filtered_list)
 
 # 2
 
@@ -19,7 +20,7 @@ def hailStone(n):
         else:
             n = (3 * n + 1)
     else:
-        print(1)
+        print(n)
 
 # 3
 
@@ -46,26 +47,30 @@ def proteins(rna, codon_map):
 
 
 def tenkSteps(stepData):
-    total = [0, 0, 0, 0, 0]
+    day_total = []
     counter = 0
-    for employee in stepData:
-        day_index = 0
-        for day in employee:
-            total[day_index] += day
-            day_index += 1
-    for count_day in total:
+    for index in range(len(stepData)):
+        for steps in stepData[index]:
+            if index not in range(len(day_total)):
+                day_total.append(steps)
+                continue
+            day_total[index] += steps
+    for count_day in day_total:
         if count_day > 100000:
             counter += 1
-    return counter
+    return counter, day_total
 
 
 def mostSteps(stepData):
-    # List of employee totals
+    # List of employee totals for the week
     week_total = []
+    # Employee number in range of employee numbers
     for employee in range(len(stepData)):
         for day in stepData[employee]:
+            # If employee number isn't already an index, append the value
             if employee not in range(len(week_total)):
                 week_total.append(day)
                 continue
+            # Otherwise add that day to the existing value in the list
             week_total[employee] += day
     return week_total
