3e696c554601ce72619b31ab5ae4113f474988c9
Conor <conor@conorfox.com>
2020-02-11 22:08:17 +0000

    Lecture notes 11/02/20

---
+++ Other/lectures.py
@@ -0,0 +1,26 @@
+# dgddhkyjyj
+
+a = [3, 6, 3, 8, 3]
+b = [7, 6, 43, 7, 5]
+
+# alist = [max(a[i], b[i]) for i in range(len(a))]
+
+# Zip
+
+# alist = [max(pair) for pair in zip(a, b)]
+
+# print(alist)
+# for item in zip(a, b):
+#     print(item)
+
+# a = 123
+# b = 456
+
+# print(list(zip(str(a), str(b))))
+
+
+# Map
+
+# print(list(map(max, a, b)))
+
+# Lambda
