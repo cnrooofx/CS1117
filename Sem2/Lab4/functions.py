2866d2f5eebde349afcee44670fa19d6a969ffc5
Conor <conor@conorfox.com>
2020-02-20 14:59:30 +0000

    Question 5

--- Sem2/Lab4/functions.py
+++ Sem2/Lab4/functions.py
@@ -2,41 +2,38 @@
 # Author: Conor Fox 119322236
 
 # 1
+
+
 def chooseLargest(a, b):
     # return list(map(max, zip(a, b)))
     return list(map(lambda x: x, map(max, zip(a, b))))
 
 
 # 2
-def lottery():
+def read_draw():
     file = open('lotteryNumbers.txt', 'r')
-    lottery_list = []
-    for line in file.readlines():
-        line = line.strip('\n').split(' ')
-        current_draw = []
-        for number in line:
-            current_draw.append(int(number))
-        lottery_list.append(current_draw)
+    lottery_list = [[int(number) for number in line.strip(
+        '\n').split(' ')] for line in file.readlines()]
+    # for line in file.readlines():
+    #     line = line.strip('\n').split(' ')
+    #     current_draw = []
+    #     for number in line:
+    #         current_draw.append(int(number))
+    #     lottery_list.append(current_draw)
     file.close()
     return lottery_list
 
 
 # 3
-def onek_deliveries(courierData):
-    day_totals = []
+def del_1000(courierData):
+    day_totals = [0] * len(courierData[0])
     for driver in courierData:
         for day, deliveries in enumerate(driver):
-            if day not in range(len(day_totals)):
-                day_totals.append(0)
             day_totals[day] += deliveries
-    onek_days = 0
-    for day in day_totals:
-        if day >= 1000:
-            onek_days += 1
-    return onek_days
+    return len([day for day in day_totals if day >= 1000])
 
 
-def most_deliveries(courierData):
+def most_del(courierData):
     driver_max = 0
     driver_id = 0
     for driver, deliveries in enumerate(courierData):
@@ -49,6 +46,27 @@ def most_deliveries(courierData):
     return driver_id
 
 
-#4
-def totals(list1, list2):
+# 4
+def append_list():
+    list1 = [1, 2, 3]
+    list2 = [10, 20, 30]
     return list(map(lambda tup: tup[0]+tup[1], zip(list1, list2)))
+
+
+# 5
+def client_matcher(client):
+    match = []
+    file = open('clients.txt', 'r')
+    clients = {person[0]: person[1:] for person in [
+        line.strip('\n').split(',') for line in file.readlines()]}
+    for person in clients:
+        gender = clients[person][0]
+        seek_gender = clients[client][2]
+        age = range(int(clients[person][3]), int(clients[person][4]))
+        seek_age = range(int(clients[client][3]), int(clients[client][4]))
+        if person == client:
+            continue
+        elif gender == seek_gender and age in seek_age:
+            match += person
+            print(person, clients[person][0], client, clients[client][2])
+    return match
--- Sem2/Lab4/main.py
+++ Sem2/Lab4/main.py
@@ -6,20 +6,19 @@ import functions
 
 def main():
     courierData = [[87, 83, 86, 88, 83], [99, 99, 85, 85, 81],
-                   [92, 96, 80, 84, 88], [83, 90, 93, 98, 2000],
+                   [92, 25000, 80, 84, 88], [83, 90, 93, 98, 2000],
                    [81, 98, 80, 82, 80], [95, 95, 98, 84, 80],
                    [100, 89, 86, 91, 90], [95, 89, 94, 95, 86],
                    [88, 83, 92, 81, 83], [92, 99, 96, 94, 200]]
     # l1 = [1, 2, 3, 4, 5]
     # l2 = [2, 2, 9, 0, 9]
     # print(functions.chooseLargest(l1, l2))
-    # print(functions.lottery())
+    # print(functions.read_draw())
+    # print(type(functions.read_draw()[0][0]))
     # print(functions.onek_deliveries(courierData))
     # print(functions.most_deliveries(courierData))
-    list1 = [1,2,3]
-    list2 = [10,20,30]
-    print(functions.totals(list1, list2))
-    zip
+    # print(functions.append_list())
+    print(functions.client_matcher('Alison Jones'))
 
 
 if __name__ == "__main__":
