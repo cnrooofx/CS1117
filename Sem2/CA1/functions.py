48c64ef63f6090eb0fb9f59d0b21c328724c54b8
Conor <conor@conorfox.com>
2020-03-03 18:17:15 +0000

    Add docstrings and comments

--- Sem2/CA1/functions.py
+++ Sem2/CA1/functions.py
@@ -5,17 +5,26 @@
 
 # 1
 def fractions(DNA):
-    if isinstance(DNA, str):
+    """
+
+    :param DNA: descr of arg1 that has True for default value
+    :returns: some value
+    """
+    # Checks that DNA is a non-empty string
+    if isinstance(DNA, str) and DNA:
         c = 0.0
         g = 0.0
         t = 0.0
         a = 0.0
         dna_string = ''
+        # Create a new string with only the correct letters
         for char in DNA:
-            if char in ('C', 'G', 'T', 'A'):
+            if char in ['C', 'G', 'T', 'A']:
                 dna_string += char
         if dna_string != '':
+            # Length is the total number of correct letters
             length = len(dna_string)
+            # Loop through the new string counting occurrences of the letters
             for char in dna_string:
                 if char == 'C':
                     c += 1
@@ -25,6 +34,7 @@ def fractions(DNA):
                     t += 1
                 else:
                     a += 1
+            # Divide the number of characters by length to get the fraction
             c /= length
             g /= length
             t /= length
@@ -61,12 +71,11 @@ def F_while(S1, S2):
 
 # (b)
 def F_list_comp(S1, S2):
-    return [e1 for e1 in S1 if e1 in S2]
+    return [e for e in S1 if e in S2]
 
 
 # (c)
 def F_lambda(S1, S2):
-    return list(filter(lambda e1: e1 in S2, S1))
     return list(filter(lambda e: e in S2, S1))
 
 
@@ -85,22 +94,39 @@ def F_error(S1, S2):
 
 # 3
 def frequencies(s):
+    """Creates a dictionary with the count of each distinct item in a sequence
+
+    :param s: string or list to check
+    :returns: sorted dictionary
+    """
+    # Check to make sure that the input is a list or a string
     if isinstance(s, (str, list)):
         s = list(s)
+        # Sort the list so that the dictionary will be sorted
         s.sort()
+        # Initialize a dictionary with a key for unique characters in the list
         out_dict = {val: 0 for val in s}
+        # For each character, increment the dictionary value
         for val in s:
             out_dict[val] += 1
         return out_dict
-    return "input is incorrect"
+    return 'input is incorrect'
 
 
 # 4
 def firsts(s):
-    if isinstance(s, list):
+    """returns the string formed from the first occurrence of each item in s
+
+    :param s: item to check (can be str, int, list or tuple)
+    :returns: string of the first occurrence of each item in s
+    """
+    # Convert lists and tuples to strings
+    if isinstance(s, (list, tuple)):
         s = ''.join([str(item) for item in s])
     out = ''
+    # Loop through the input
     for char in str(s):
+        # Add the character to the output if it isn't already there
         if char not in out:
             out += char
     return out
@@ -108,15 +134,26 @@ def firsts(s):
 
 # 5
 def extract(text, n, m):
+    """extracts a hidden message from a passage of text
+
+    :param text: string of text containing a hidden message
+    :param n: checks the 'n'th letter of the words
+    :param m: checks every 'm'th word
+    :returns: the hidden message
+    """
     hidden = ''
     text = text.split()
     i = int(m)
     while i < len(text):
         word = text[i-1]
+        # Strip multiple times to remove different punctuation marks
         for to_strip in ('.', ',', '!', '?'):
             word.strip(to_strip)
+        # Error if the index is greater than the length of the current word
         if (n-1) not in range(len(word)):
             return 'There is no character %s in the word - %s' % (n, word)
+        # Add the required letter of the current word to the output
         hidden += word[n-1]
+        # Increment the counter by m to check the next word
         i += m
     return hidden
--- Sem2/CA1/main.py
+++ Sem2/CA1/main.py
@@ -13,6 +13,7 @@ def main():
     # print(functions.fractions("1"))  # ⇒ (0.0,0.0,0.0,0.0)
     # print(functions.fractions(1))  # ⇒ "input must be a string character"
     # print(functions.fractions('OTTACACTTATTTTTTC'))
+    # print(functions.fractions(''))
     # print('\nF\n')
     # S1 = [105, 101, 105, 100, 105]
     # S2 = [100, 102, 101, 102, 101]
@@ -20,17 +21,26 @@ def main():
     # print(functions.F_while(S1, S2))
     # print(functions.F_list_comp(S1, S2))
     # print(functions.F_lambda(S1, S2))
+    # print(functions.F_error(S1, S2))
+    # print()
+    # S3 = [254, 255, 255, 260, 256, 256, 255, 253, 259, 250]
+    # S4 = [256, 259, 260, 256, 256, 250]
+    # print(functions.F(S3, S4))
+    # print(functions.F_while(S3, S4))
+    # print(functions.F_list_comp(S3, S4))
+    # print(functions.F_lambda(S3, S4))
+    # print(functions.F_error(S3, S4))
     # print('\nFrequencies\n')
     # print(functions.frequencies("CCABBADCAC"))  # ⇒ {"A":3, "B":2, "C":4, "D":1}
     # print(functions.frequencies([4, 7, 4, 7, 4]))  # ⇒ {4:3, 7:2}
     # print(functions.frequencies(1))  # ⇒ "input is incorrect"
-    print('\nFirsts\n')
+    # print('\nFirsts\n')
     # print(functions.firsts("mississippi"))  # ⇒ "misp"
     # print(functions.firsts("abcdefg"))  # ⇒ " abcdefg "
     # print(functions.firsts("aaaaaaaa"))  # ⇒ "a"
     # print(functions.firsts(""))  # ⇒ ""
     # print(functions.firsts(123454321))  # ⇒ "12345"
-    print(functions.firsts([1, 2, 3, 4, 5, 4, 3, 2, 1]))
+    # print(functions.firsts([1, 2, 3, 4, 5, 4, 3, 2, 1]))
     # print('\nExtract\n')
     # print(functions.extract("Yesterday I saw an aardvark while walking my pet tortoise, Frank. What a sight this was! Aardvarks are nocturnal animals appearing in daylight with caution. Make sure to bring kippers when you visit", 1, 5))
 
