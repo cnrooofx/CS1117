04e8392dc7e9bdd680f29781f5c05b321a98c4ad
Conor <conor@conorfox.com>
2020-03-03 14:33:17 +0000

    Docstring example

---
+++ Template/docstring.py
@@ -0,0 +1,9 @@
+
+def func(arg1, arg2='default val'):
+    """Description of func with docstring groups style.
+
+    :param arg1: descr of arg1 that has True for default value
+    :param arg2: descr of arg2 (Default value = 'default val')
+    :returns: some value
+    """
+    return
