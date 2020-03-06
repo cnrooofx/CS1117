73a989b7f7b1ea4df77e4df9cbe8a0748b2d82f4
Conor <conor@conorfox.com>
2020-03-06 20:01:04 +0000

    Questions 1 & 2

--- Sem2/Lab5/functions.py
+++ Sem2/Lab5/functions.py
@@ -2,14 +2,22 @@
 # Author: Conor Fox 119322236
 
 
+# 1
+def factorialL(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorialL(n-1)
+
+
 # 2
 def hailStoneR(n):
-    print(n)
+    start = [n]
     if n == 1:
-        return
+        return [1]
     elif n % 2 == 0:
         n //= 2
     else:
         n = (3 * n + 1)
-    hailStoneR(n)
+    return start + hailStoneR(n)
 
