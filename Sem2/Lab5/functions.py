c10d28ac10ab53f6304eccde6a5b920039a26810
Conor <conor@conorfox.com>
2020-03-27 14:35:36 +0000

    Some clean up, adding try except & comments

--- Sem2/Lab5/functions.py
+++ Sem2/Lab5/functions.py
@@ -15,14 +15,14 @@ def factorialL(n):
 
 # 2
 def hailStoneR(n):
-    start = [n]
+    current_num = [n]
     if n == 1:
         return [1]
     elif n % 2 == 0:
         n //= 2
     else:
         n = (3 * n + 1)
-    return start + hailStoneR(n)
+    return current_num + hailStoneR(n)
 
 
 # 3
@@ -34,32 +34,43 @@ def chooseLargest(a, b):
     print(results)
 
 
-def chooseLargest_comp(a, b):
+def chooseLargest(a, b):
     return [max(a[i], b[i]) for i in range(len(a))]
 
 
-def chooseLargest_lambda(a, b):
+def chooseLargest(a, b):
     return list(map(lambda x: max(x), zip(a, b)))
 
 
 # 4
 def redact():
-    sensitive_words = open('sensitiveWords.txt', 'r')
-    words = [word.strip('\n').lower() for word in sensitive_words.readlines()]
-    sensitive_words.close()
-    out = open('redacted.txt', 'w')
-    to_redact = open('unredacted.txt', 'r')
-    for line in to_redact.readlines():
-        current_line = ''
-        for word in line.split():
-            if word.lower() in words:
-                current_line += (('*' * len(word)) + ' ')
-            else:
-                current_line += (word + ' ')
-        out.write(current_line + '\n')
-    print(words)
-    out.close()
-    to_redact.close()
+    try:
+        # Create a list of the sensitive words from the file
+        sensitive = open('sensitiveWords.txt', 'r')
+        words = [word.strip('\n').lower() for word in sensitive.readlines()]
+        sensitive.close()
+        # Create file to write redacted text to
+        out = open('redacted.txt', 'w')
+        to_redact = open('unredacted.txt', 'r')
+        # Loop through every line in the file to redact
+        for line in to_redact.readlines():
+            # Create a new line to write into the new file
+            current_line = ''
+            # Loop through every word in the line
+            for word in line.split():
+                # Check to see if it's a sensitive word
+                if word.lower() in words:
+                    # Replace with the same amount of asterixes as the length
+                    current_line += (('*' * len(word)) + ' ')
+                else:
+                    # Otherwise just add the word
+                    current_line += (word + ' ')
+            # After every line, write it to the redacted file.
+            out.write(current_line + '\n')
+        out.close()
+        to_redact.close()
+    except IOError:
+        return "Error! File 'unredacted.txt' or 'sensitiveWords.txt' not found"
 
 
 # 5
@@ -81,9 +92,8 @@ def redact():
     converted into a list which is then printed.
 """
 
+
 # 6
-
-
 def sum_it():
     summation = []
     for n in range(17):
@@ -112,20 +122,37 @@ def shuffle(deck):
 
 # 8
 def word_count():
-    book = open('book.txt', 'r')
-    wc = {}
-    for line in book.readlines():
-        for word in line.strip('\n').split():
-            word = ''.join(l for l in word if l not in string.punctuation)
-            word = word.lower()
-            if word not in wc:
-                wc[word] = 1
-            else:
-                wc[word] += 1
-    most = 0
-    most_word = ''
-    for key in wc:
-        if wc[key] > most:
-            most = wc[key]
-            most_word = key
-    return most_word
+    try:
+        book = open('book.txt', 'r')
+        # Dictionary to keep track of word counts
+        wc = {}
+        # Loop through every line in the book
+        for line in book.readlines():
+            # Break up each line by word
+            for word in line.strip('\n').split():
+                # Filter out the punctuation
+                word = ''.join(l for l in word if l not in string.punctuation)
+                # Make it lowercase to be consistent
+                word = word.lower()
+                # If the word is not already in the dictionary, initialise it
+                if word not in wc:
+                    wc[word] = 1
+                else:
+                    # Otherwise increment the counter for that word
+                    wc[word] += 1
+        # Counter for the most amount of occurrences
+        most = 0
+        # That word which appears most often
+        most_word = ''
+        # Loop through the counter dictionary
+        for key in wc:
+            # Compare that counter to the current highest value
+            if wc[key] > most:
+                # Assign that total to be the new highest value found
+                most = wc[key]
+                # Save that word with the highest total
+                most_word = key
+        book.close()
+        return most_word
+    except IOError:
+        return "Error! File 'book.txt' not found."
