8eca5f6792ffdca8e9255b576bf003078d106d7d
Conor <conor@conorfox.com>
2020-04-07 09:29:10 +0100

    Question 2 - Who am I

--- Sem2/CA2/functions.py
+++ Sem2/CA2/functions.py
@@ -27,3 +27,27 @@ def what_am_I(input_list, my_type='all'):
         return out
     except:
         return 'Error'
+
+
+def who_am_I(file_input):
+    try:
+        my_file = open(file_input, 'r')
+        type_list = []
+        for line in my_file.readlines():
+            for word in line.strip('\n').strip('[').strip(']').split(','):
+                word = word.strip()
+                print(word)
+                if word == 'True':
+                    type_list.append(True)
+                elif word == 'False':
+                    type_list.append(False)
+                elif (word[0] == '\'' and word[-1] == '\'') or \
+                     (word[0] == '\"' and word[-1] == '\"'):
+                    type_list.append(word.strip('\'').strip('\"'))
+                elif '.' in word:
+                    type_list.append(float(word))
+                else:
+                    type_list.append(int(word))
+        return what_am_I(type_list, my_type='all')
+    except:
+        return 'Error'
--- Sem2/CA2/main.py
+++ Sem2/CA2/main.py
@@ -5,11 +5,13 @@ from functions import *
 
 
 def main():
-    print(what_am_I(['hi', 'conor', 123, 0.123, [1, 2], ('hi', 'there')], my_type='all'))
-    print(what_am_I(['hi', 'conor', 123, 0.123, [1, 2], ('hi', 'there')], my_type='bob'))
-    print(what_am_I([]))
-    input_list = [1, "It's", 1.2, "fun", {"hi": "you"}, "time", 2]
-    print(what_am_I(input_list, my_type=str))  # ⇒ {'str': ["It's", 'fun', 'time']}
+    # print(what_am_I(['hi', 'conor', 123, 0.123, [1, 2], ('hi', 'there')], my_type='all'))
+    # print(what_am_I(['hi', 'conor', 123, 0.123, [1, 2], ('hi', 'there')], my_type='bob'))
+    # print(what_am_I([]))
+    # input_list = [1, "It's", 1.2, "fun", {"hi": "you"}, "time", 2]
+    # print(what_am_I(input_list, my_type=str))  # ⇒ {'str': ["It's", 'fun', 'time']}
+    print(who_am_I('file.txt'))
+    print(who_am_I("input.txt"))  # ⇒ {'int': [1, 2], 'float': [2.4, 1.4], 'bool': [True, False], 'str': ['Oops', 'OK']}
 
 
 
