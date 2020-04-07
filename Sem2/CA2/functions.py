0fb72a6b4c3b6b9467fd4e38e059941d02b70d31
Conor <conor@conorfox.com>
2020-04-07 14:45:58 +0100

    Add comments and docstrings

--- Sem2/CA2/functions.py
+++ Sem2/CA2/functions.py
@@ -4,32 +4,45 @@
 
 
 def what_am_I(input_list, my_type='all'):
+    """Creates a dictionary from items in a list by type
+
+    :param input_list: list of various Python types
+    :param my_type: Python type to filter by (Default value = 'all')
+    :returns: dictionary with the types of the list elements as keys
+    """
     try:
         out = {}
+        # If the my_type is a string and isn't all, return an empty dictionary
         if my_type != 'all' and isinstance(my_type, str):
             return out
         elif len(input_list) > 0:
-            if my_type != 'all':
-                my_type = str(my_type.__name__)
-                for val in input_list:
-                    if type(val).__name__ == my_type:
-                        if my_type not in out:
-                            out[my_type] = [val]
-                        else:
-                            out[my_type] += [val]
+            if my_type == 'all':
+                all = True
             else:
-                for val in input_list:
-                    my_type = type(val).__name__
-                    if my_type not in out:
-                        out[my_type] = [val]
-                    else:
-                        out[my_type] += [val]
+                # If my_type isn't all, add that type to the dictionary
+                out[str(my_type.__name__)] = []
+                # Set all to False, stops other types being added to dictionary
+                all = False
+            for val in input_list:
+                # Get the type of the current item in the list
+                my_type = type(val).__name__
+                # If the type is already in the dictionary, add it to that list
+                if my_type in out:
+                    out[my_type] += [val]
+                # Only add other types to the dictionary if all is set to True
+                elif all:
+                    out[my_type] = [val]
         return out
     except:
         return 'Oops, there was an error in what_am_I'
 
 
 def who_am_I(file_input):
+    """Creates a dictionary from items in a file by type
+
+    :param file_input: name of file containing lists of various Python types
+    :returns: dictionary with the types of the list elements as keys
+    """
     try:
         my_file = open(file_input, 'r')
         type_list = []
@@ -53,8 +66,14 @@ def who_am_I(file_input):
 
 
 def matrices(file_input):
+    """Creates a matrix taking input from a file
+
+    :param file_input: name of file containing the matrix
+    :returns: matrix (list of lists) from the file
+    """
     try:
         matrix = []
+        # Read the file and create the matrix from it
         with open(file_input, 'r') as my_file:
             for line in my_file.readlines():
                 cur_line = []
@@ -62,9 +81,12 @@ def matrices(file_input):
                     num = num.strip()
                     cur_line.append(int(num))
                 matrix.append(cur_line)
+        # Create an empty matrix to be the reversed version
         rev_matrix = [[] for i in range(len(matrix))]
         for row in matrix:
+            # Get the index and value in each row
             for ind, val in enumerate(row):
+                # Add value to the column at that index in the reversed matrix
                 rev_matrix[ind].append(val)
         return rev_matrix
     except:
@@ -72,13 +94,23 @@ def matrices(file_input):
 
 
 def sum_me(matrices_list):
+    """Sums up rows and columns in a matrix
+
+    :param matrices_list: matrix (list of lists)
+    :returns: tuple with the sum of all the rows and the sum of all the columns
+    """
     try:
-        rows = [0 for i in range(len(matrices_list))]
-        columns = [0 for i in range(len(matrices_list[0]))]
-        for row_ind, row in enumerate(matrices_list):
-            for col_ind, val in enumerate(row):
-                rows[row_ind] += val
-                columns[col_ind] += val
+        # Create lists to sum up the values
+        rows = [0 for i in range(len(matrices_list[0]))]
+        columns = [0 for i in range(len(matrices_list))]
+        # Get the index of every row
+        for row_index, row in enumerate(matrices_list):
+            # Get the index of every value in each row
+            for col_index, val in enumerate(row):
+                # Add the value to the current row index in the sum list
+                rows[row_index] += val
+                # Add the value to the current column index in the sum list
+                columns[col_index] += val
         return rows, columns
     except:
         return 'Oops, there was an error in sum_me'
